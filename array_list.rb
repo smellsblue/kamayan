b4a8d6a547f275e189b6643c7c3fdc9b7cff93da
Mike Virata-Stone <mjstone@on-site.com>
2016-09-15 23:45:44 -0700

    Solve 1-3

--- array_list.rb
+++ array_list.rb
@@ -11,36 +11,41 @@ class ArrayList
     @array[index]
   end
 
-  # Define a method "<<" which takes a single argument. This method should
-  # append the argument to the end of this ArrayList and increase the size by
-  # 1. The return value must be self.
+  def <<(value)
+    self[size] = value
+    self
+  end
 
-  # Define a method ">>" which takes a single argument. This method should
-  # prepend the argument to the beginning of this ArrayList and increase the
-  # size by 1. The return value must be self.
+  def >>(value)
+    resize_array(size + 1)
+    (size - 1).downto(0) { |i| @array[i + 1] = @array[i] }
+    @array[0] = value
+    @size += 1
+    self
+  end
 
-  # Define a "delete" method which takes a single index argument. This method
-  # should delete the value at the provided index and return it. The size should
-  # be 1 less than it was before this method was called. The index must be
-  # within the bounds of the ArrayList, or an IndexError should be raised.
+  def delete(index)
+    result = self[index]
+    index.upto(size - 1) { |i| @array[i] = @array[i + 1] }
+    @size -= 1
+    result
+  end
 
-  # Define a method "[]=" which takes 2 arguments. This method should set the
-  # value at the index defined in the first argument such that array_list[index]
-  # will return the second argument.
-  #
-  # If the index is negative, an IndexError should be raised.
-  #
-  # If the index is bigger than the current size of the @array, the @array
-  # should be replaced with a bigger FixedArray to fit the new index. All
-  # indexes between the former last element and the new index should be
-  # initialized with nil. An additional buffer should be included in the new
-  # FixedArray (in case the array is grown more), though this is not required.
-  #
-  # The size after this method is called depends on the index provided. An
-  # existing index would not affect the size, but an index greater than the last
-  # index will add the difference to the size.
+  def []=(index, value)
+    check_lower_bound(index)
+    resize_array(index + 1)
+    @array[index] = value
+    @size = index + 1 if index >= size
+  end
 
   private
+
+  def resize_array(required_size)
+    return if @array.size >= required_size
+    new_array = FixedArray.new(required_size + 10)
+    0.upto(@array.size - 1) { |i| new_array[i] = @array[i] }
+    @array = new_array
+  end
 
   def check_bounds(index)
     check_lower_bound(index)
--- linked_list.rb
+++ linked_list.rb
@@ -19,38 +19,76 @@ class LinkedList
     end
   end
 
-  # Define a method ">>" which takes a single argument. This method should
-  # prepend the argument to the beginning of this LinkedList and increase the
-  # size by 1. The return value must be self.
+  def >>(value)
+    if @head
+      new_head = LinkedList::Node.new(value, @head)
+      @head = new_head
+    else
+      @head = LinkedList::Node.new(value)
+    end
 
-  # Define a method "<<" which takes a single argument. This method should
-  # append the argument to the end of this LinkedList and increase the size by
-  # 1. The return value must be self.
+    @size += 1
+    self
+  end
 
-  # Define a "delete" method which takes a single index argument. This method
-  # should delete the value at the provided index and return it. The size should
-  # be 1 less than it was before this method was called. The index must be
-  # within the bounds of the LinkedList, or an IndexError should be raised.
+  def <<(value)
+    if @head
+      find_node(size - 1).child = LinkedList::Node.new(value)
+    else
+      @head = LinkedList::Node.new(value)
+    end
 
-  # Define a method "[]" which takes a single index argument. This method should
-  # retrieve the value at the given index. The index must be within the bounds
-  # of the LinkedList, or an IndexError should be raised.
+    @size += 1
+    self
+  end
 
-  # Define a method "[]=" which takes 2 arguments. This method should set the
-  # value at the index defined in the first argument such that
-  # linked_list[index] will return the second argument.
-  #
-  # If the index is negative, an IndexError should be raised.
-  #
-  # If the index is bigger than the current size of the linked list, the links
-  # should be adjusted to fit the new index.  All indexes between the former
-  # last element and the new index should be initialized with nil.
-  #
-  # The size after this method is called depends on the index provided. An
-  # existing index would not affect the size, but an index greater than the last
-  # index will add the difference to the size.
+  def delete(index)
+    check_bounds(index)
+
+    if index == 0
+      result = @head.value
+      @head = @head.child
+    else
+      prev = find_node(index - 1)
+      result = prev.child.value
+      prev.child = prev.child.child
+    end
+
+    @size -= 1
+    result
+  end
+
+  def [](index)
+    check_bounds(index)
+    find_node(index).value
+  end
+
+  def []=(index, value)
+    check_lower_bound(index)
+    self << nil if @head.nil?
+
+    if index >= size
+      last_node = find_node(size - 1)
+
+      while index >= size
+        last_node.child = LinkedList::Node.new(nil)
+        last_node = last_node.child
+        @size += 1
+      end
+
+      last_node.value = value
+    else
+      find_node(index).value = value
+    end
+  end
 
   private
+
+  def find_node(index)
+    node = @head
+    0.upto(index - 1) { node = node.child }
+    node
+  end
 
   def check_bounds(index)
     check_lower_bound(index)
